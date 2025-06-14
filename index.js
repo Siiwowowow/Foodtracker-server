4b43db015e238378fcefdb44b0bfb278170d7047
Siiwowowow <siaa32290@gmail.com>
2025-06-14 22:41:05 +0600

    local host add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:['http://localhost:5174'],
   credentials:true
 }));
 app.use(express.json());
